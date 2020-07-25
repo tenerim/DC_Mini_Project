edc3520e1a2fa32330041ab053d1dc1df547756c
Michael Tenerife <michaeltenerife@hotmail.com>
2020-07-25 16:57:53 +0000

    Adding a Task & Writing to the DB

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
@@ -9,13 +9,26 @@ app.config["MONGO_URI"] = 'mongodb+srv://root:Crack007@myfirstcluster.fwdpj.mong
 
 mongo = PyMongo(app)
 
-
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template("tasks.html", 
+                           tasks=mongo.db.tasks.find())
 
 
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html',
+                           categories=mongo.db.categories.find())
+
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+    
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
